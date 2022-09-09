b6699d686844f3b475ad532663b018b361d81d16
MrLucio1 <azakari180@gamil.com>
2022-09-08 19:44:21 -0700

    Such a

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <string.h>
 #include <unistd.h>
 /**
  * main - main block
@@ -7,6 +5,6 @@
  */
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
