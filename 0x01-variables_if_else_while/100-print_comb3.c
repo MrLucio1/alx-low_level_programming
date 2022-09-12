584cd205679236ffde4b0cc35058179789e68ac2
MrLucio1 <azakari180@gamil.com>
2022-09-12 07:40:07 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,30 +1,17 @@
 #include <stdio.h>
 /**
- * main - program that prints the numbers from 01 to 89
- * Numbers must be separated by , followed by a space
- * Numbers should be printed in ascending order with 2 digits
- * You can only usethe putchar function
- * Return: 0
+ * main - prints all single digit numbers of base 10 starting from 0'
+ * followed by a new line.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int num1, num2;
+	int num;
 
-	for (num1 = 48; num1 <= 56; num1++)
+	for (num = 48; num < 58; num++)
 	{
-		for (num2 = 49; num2 <= 57; num2++)
-		{
-			if (num2 > num1)
-			{
-				putchar(num1);
-				putchar(num2);
-				if (num1 != 56 || num2 != 57)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-		}
+		putchar(num);
+
 	}
 	putchar('\n');
 	return (0);
