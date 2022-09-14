a3ccb8b0cd9e4330a4b0f7cba4b7e6914ec8a069
MrLucio1 <azakari180@gamil.com>
2022-09-13 23:40:03 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <main.h>
+/**
+ * main - print _putchar, followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	char ch = 'A';
+	_putchar('\n');
+		putchar(ch);
+	return (0);
+}
+
