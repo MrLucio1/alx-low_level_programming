ac3f831a8a063821839671d9a30dc7e59ec57e94
MrLucio1 <azakari180@gamil.com>
2022-09-14 05:07:28 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,12 +2,18 @@
 /**
  * main - print _putchar, followed by a new line
  * Return: 0
- */
-int main(void)
-{
-	_putchar('A');
-	_putchar('ch');
-	_putchar('\n');
-	return (0);
-}
+ *i/
+ int main(void)
+ {
+         _putchar('_');
+         _putchar('p');
+         _putchar('u');
+         _putchar('t');
+         _putchar('c');
+         _putchar('h');
+         _putchar('a');
+         _putchar('r');
+         _putchar('\n');
 
+         return (0);
+ }
