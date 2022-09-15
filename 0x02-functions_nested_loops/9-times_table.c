ad8400f04e02abd1a1c32b322b27c7890d8ab9eb
MrLucio1 <azakari180@gamil.com>
2022-09-14 21:54:03 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,6 +6,7 @@
  * Example Table
  * 0, 0, 0, 0, ..
  * 0, 1, 2, 3, ..
+ * Return: 0
  */
 
 void times_table(void)
@@ -29,9 +30,11 @@ void times_table(void)
 			if (prod <= 9)
 				_putchar(' ');
 			else
-				_putchar((prod / 10) + 48); /*get the first digit*/
+				_putchar((prod / 10) + 48);
+			/*get the first digit*/
 
-			_putchar((prod % 10) + 48); /*get the second digit*/
+			_putchar((prod % 10) + 48);
+			/*get the second digit*/
 		}
 		_putchar('\n');
 	}
