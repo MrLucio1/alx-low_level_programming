866ad166d0acb770cca81069d066ff08d054c5ad
MrLucio1 <azakari180@gamil.com>
2022-09-19 21:08:13 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * Reset_to_98 - updates the value of the variable
+ * The pointer points to 98
+ * @n: the pointer to the variable is up to date
+ */
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is a C - Pointers, arrays and strings project
