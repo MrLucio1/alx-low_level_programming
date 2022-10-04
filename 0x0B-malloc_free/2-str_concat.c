dce864416d132a5e298b10c783412c0d0d10a62e
MrLucio1 <azakari180@gamil.com>
2022-10-04 04:18:04 -0700

    5th time

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -6,20 +6,20 @@
  * @s1: first string to concatenate
  * @s2: second string to be concatenated
  *
- * Return: pointer to the new string created (Success),
+ * Return: pointer to string created (Success)
  * NULL (Error)
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *s3;
-	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+	char *n1;
+	unsigned int i = 0, j = 0, num1 = 0, num2 = 0;
 
-	while (s1 && s1[len1])
-		len1++;
-	while (s2 && s2[len2])
-		len2++;
-	s3 = malloc((len1 + len2 + 1) * sizeof(char));
-	if (s3 == NULL)
+	while (s1 && s1[num1])
+		num1++;
+	while (s2 && s2[num2])
+		num2++;
+	n1 = malloc((num1 + num2 + 1) * sizeof(char));
+	if (n1 == NULL)
 		return (NULL);
 
 	i = 0;
@@ -27,21 +27,21 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1)
 	{
-		while (i < len1)
+		while (i < num1)
 		{
-			s3[i] = s1[i];
+			n1[i] = s1[i];
 			i++;
 		}
 	}
 	if (s2)
 	{
-		while (i < (len1 + len2))
+		while (i < (num1 + num2))
 		{
-			s3[i] = s2[i];
+			n1[i] = s2[i];
 			i++;
 			j++;
 		}
 	}
-	s3[i] = '\0';
-	return (s3);
+	n1[i] = '\0';
+	return (n1);
 }
