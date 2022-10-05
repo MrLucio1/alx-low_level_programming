c1e81f7aac6c08b38193905d2691c8e56b3174a9
MrLucio1 <azakari180@gamil.com>
2022-10-05 01:21:01 -0700

    Without libraries what have we? We have no past and no future

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* _strlen - prints the length of a string
+* @s: This is the string to be evaluated
+*
+* Return: the length of the string
+*/
+int _strlen(char *s)
+{
+int i;
+
+i = 0;
+
+while (s[i] != '\0')
+{
+i++;
+}
+return (i);
+}
