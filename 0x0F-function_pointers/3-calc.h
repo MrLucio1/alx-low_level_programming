690ff8f95ff78450a68bf7a70dbb1f9147b006ec
MrLucio1 <azakari180@gamil.com>
2022-10-13 10:49:19 -0700

    Function pointers

---
+++ 0x0F-function_pointers/3-calc.h
@@ -0,0 +1,27 @@
+#ifndef CALC_H
+#define CALC_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct op
+{
+	char *op;
+	int (*f)(int a, int b);
+} op_t;
+
+int op_add(int i, int j);
+int op_sub(int i, int j);
+int op_mul(int i, int j);
+int op_div(int i, int j);
+int op_mod(int i, int j);
+int (*get_op_func(char *s))(int, int);
+
+#endif
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+This is a project that deals with function pointers for alx task
